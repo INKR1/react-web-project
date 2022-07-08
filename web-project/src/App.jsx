47ab36a8fc2171d9a8c6b2e208dbefb37afd9a96
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-08 14:50:49 +0300

    Adding Routing

--- web-project/src/App.jsx
+++ web-project/src/App.jsx
@@ -1,8 +1,23 @@
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import AllMeetups from './pages/AllMeetups';
+import NewMeetup from './pages/NewMeetup';
+import Favorites from './pages/Favorites';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
 
     </div>
   );
---
+++ web-project/src/pages/NewMeetup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewMeetup() {
+  return (
+    <div>
+      New meet up page
+    </div>
+  )
+}
