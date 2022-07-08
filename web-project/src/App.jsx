0c9933905e135ed2674761b1222ed0ddb610cda0
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-08 14:58:36 +0300

    Adding links and navigation

--- web-project/src/App.jsx
+++ web-project/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import Favorites from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <MainNavigation />
       <Switch>
         <Route exact path="/">
           <AllMeetups />
