aa84e4bbbf3ac112aca8b6f728479d1188487428
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-10 19:38:08 +0300

    Favorites page

--- web-project/src/pages/Favorites.jsx
+++ web-project/src/pages/Favorites.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { useContext } from "react"
+
+import FavoritesContext from "../store/favorites-context"
+import CitiesList from "../components/cities/CitiesList"
 
 export default function Favorites() {
+
+  const favCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if(favCtx.favorites.length === 0) {
+    content = <p>You got no favorites yet</p>
+  } else {
+    content = <CitiesList cities={favCtx.favorites} />
+
+  }
+
   return (
-    <div>
-        Favorites page
-      
-    </div>
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
   )
 }
